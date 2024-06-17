8415440550c9143513253648d8ed20b4765d9678
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2024-06-17 10:09:02 -0400

    Update binding for Polkadot API

--- contracts/bindings/IncredibleSquaringTaskManager/binding.go
+++ contracts/bindings/IncredibleSquaringTaskManager/binding.go
@@ -71,6 +71,7 @@ type IIncredibleSquaringTaskManagerTask struct {
 type IIncredibleSquaringTaskManagerTaskResponse struct {
 	ReferenceTaskIndex uint32
 	NumberSquared      *big.Int
+	LastDigitOfBlockExtrinsic int
 }
 
 // IIncredibleSquaringTaskManagerTaskResponseMetadata is an auto generated low-level Go binding around an user-defined struct.
